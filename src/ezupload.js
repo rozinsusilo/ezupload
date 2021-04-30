1b26f696f5be2cfb1dbbc7fc3703c92b2f5b51b4
Rozin Susilo <olisusnizor@gmail.com>
2021-04-30 14:49:41 +0700

    add function to add file and show it to list

--- src/ezupload.js
+++ src/ezupload.js
@@ -16,14 +16,33 @@
     $.fn.ezupload.defaults = {
         multiple: true,
         allowedExtension: [],
-        showSize: false
+        maxFileSize: null,
+        showSize: false,
+        width: "100%",
+        name: "files"
     }
 
-    function build (el) {
-        const html = `<div class="ezupload p-4 bg-secondary">
-        <div class="ez-header"><button class="btn btn-primary">Add File</button></div>    
-        <div class="ez-list></div>
-        </div>`
-        $(html).appendTo(el)
+    function build(el) {
+        const html = $(`<div class="ezupload">
+        <div class="ez-header"></div>    
+        <div class="ez-list"></div>
+        </div>`)
+        const addFileButton = $(`<button class="ez-button">Add File</button>`)
+        addFileButton.on('click', function() {
+            const newFile = $(`<input type="file" name="" class="ez-input-file">`)
+            newFile.trigger('click').on("change", function(){
+                const newFileHtml = $(`<div class="ez-list-item"></div>`)
+                const fileInfo = $(`<div><h3 class="ez-item-header">Filename</h3><small>50 MB</small></div>`)
+                const deleteFileButton = $(`<button class="ez-delete-item"></button>`)
+                newFileHtml.append(fileInfo, deleteFileButton).appendTo()
+                html.find('.ez-list').append(newFileHtml)
+            })
+        })
+        html.find('.ez-header').append(addFileButton)
+        el.append(html)
+    }
+
+    function formatSize() {
+
     }
 }(jQuery))
